1ac2fb16541fe6837e776bb8600b6964c02bde56
gchinfatt <gchinfatt@github.com>
2015-09-23 19:33:01 -0400

    Initial Visual Studio Solution

---
+++ daniel-incident-tracking/daniel-incident-tracking.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace daniel_incident_tracking.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ daniel-incident-tracking/daniel-incident-tracking/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="daniel_incident_tracking.MvcApplication" Language="C#" %>
